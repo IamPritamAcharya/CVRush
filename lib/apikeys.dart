57db6fd4804fcc92b57ad77401e5daac47a83ee1
Pritam Acharya <pritamach.exe@gmail.com>
2025-03-18 17:31:40 +0530

    Update apikeys.dart

--- lib/apikeys.dart
+++ lib/apikeys.dart
@@ -1,11 +1,11 @@
 
 // Supabase - main.dart
     
-String url =  'https://qbqimhasoqroecnzhvrb.supabase.co';
-String anonKey =  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFicWltaGFzb3Fyb2VjbnpodnJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk2NzQ5MjUsImV4cCI6MjA1NTI1MDkyNX0.pFp3gmYRXSJK5E6W65Xe2aH0t9zpN2L3eIlBrcy7B3g'; 
+String url =  '';
+String anonKey =  ''; 
 
 // sign in
 
-String webClientId = '669920565478-rfq2aae8idiuqoif5ft9n361iau629nc.apps.googleusercontent.com';
+String webClientId = '';
 
 
